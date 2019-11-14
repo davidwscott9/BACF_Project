36f1a6b85086dc341707614ea30d421d1510ce74
ddog <davidwscott9@gmail.com>
2019-11-14 14:36:48 -0500

    Added additional MATLAB hog_images to run to frame=2

--- hog_matlab.py
+++ hog_matlab.py
@@ -25,4 +25,30 @@ def hog_matlab(k):
         hog_image = sio.loadmat('hog_image5.mat')
         hog_image = np.array(hog_image['hog_image5'])
 
+    if k == 6:
+        hog_image = sio.loadmat('hog_image6.mat')
+        hog_image = np.array(hog_image['hog_image6'])
+
+    if k == 7:
+        hog_image = sio.loadmat('hog_image7.mat')
+        hog_image = np.array(hog_image['hog_image7'])
+
+    if k == 8:
+        hog_image = sio.loadmat('hog_image8.mat')
+        hog_image = np.array(hog_image['hog_image8'])
+    if k == 9:
+        hog_image = sio.loadmat('hog_image9.mat')
+        hog_image = np.array(hog_image['hog_image9'])
+
+    if k == 10:
+        hog_image = sio.loadmat('hog_image10.mat')
+        hog_image = np.array(hog_image['hog_image10'])
+
+    if k == 11:
+        hog_image = sio.loadmat('hog_image11.mat')
+        hog_image = np.array(hog_image['hog_image11'])
+
+    if k == 12:
+        hog_image = sio.loadmat('hog_image12.mat')
+        hog_image = np.array(hog_image['hog_image12'])
     return hog_image
