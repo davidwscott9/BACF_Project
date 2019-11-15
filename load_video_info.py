c76a41a765e551ff6c039bcc590a774791f408a4
ddog <davidwscott9@gmail.com>
2019-11-15 12:38:38 -0500

    Now accepts tab delimited

--- load_video_info.py
+++ load_video_info.py
@@ -7,12 +7,17 @@ def load_video_info(video_path):
 
     lines = open(file_str).read().splitlines()
     ground_truth = np.array(lines[0].split(","))
+    # length should be 4. If not, didn't separate data properly. Try another delimiter type
+    if len(ground_truth) == 1:
+        ground_truth = np.array(lines[0].split("\t"))  # in case tab delimited values instead of comma
     for i in range(1, len(lines)):
         row = np.array(lines[i].split(","))
+        # length should be 4. If not, didn't separate data properly. Try another delimiter type
+        if len(row) == 1:
+            row = np.array(lines[i].split("\t"))  # in case tab delimited values instead of comma
         ground_truth = np.vstack((ground_truth, row))
 
     ground_truth = ground_truth.astype('float64')
-
     gt_len = ground_truth.shape[0]
     init_rect = ground_truth[0, :]
 
