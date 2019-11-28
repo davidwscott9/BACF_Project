4af7262cb6f742afeab601408e28bdc694c403fe
ddog <davidwscott9@gmail.com>
2019-11-28 10:54:48 -0500

    Bug fixes

--- get_pixels.py
+++ get_pixels.py
@@ -6,10 +6,10 @@ def get_pixels(im, pos, sz, resize_target):
     ys = np.floor(pos[0]) + np.arange(0, sz[0]) - np.floor(sz[0] / 2)
 
     # check for out-of-bounds coordinates, and set them to the values at the borders
-    xs[xs < 1] = 1
-    ys[ys < 1] = 1
-    xs[xs > im.shape[1]] = im.shape[1]
-    ys[ys > im.shape[0]] = im.shape[0]
+    xs[xs < 1] = 0
+    ys[ys < 1] = 0
+    xs[xs >= im.shape[1]] = im.shape[1] - 1
+    ys[ys >= im.shape[0]] = im.shape[0] - 1
 
     # extract image
     im_patch = im[int(min(ys)):int(max(ys))+1, int(min(xs)):int(max(xs)+1), :]
