95aec64fd559db0991d130023e9f0e4b0fe94052
ddog <davidwscott9@gmail.com>
2019-11-28 21:12:28 -0500

    Accounts for div by 0 case

--- computePascalScore.py
+++ computePascalScore.py
@@ -3,6 +3,10 @@ def computePascalScore(bb1, bb2):
     from computeIntersectionArea import computeIntersectionArea
     # compute the Pascal score of the bb1, bb2 (intersection/union)
     intersectionArea = computeIntersectionArea(bb1,bb2)
-    pascalScore = intersectionArea/(computeArea(bb1)+computeArea(bb2)-intersectionArea)
+
+    if (computeArea(bb1)+computeArea(bb2)-intersectionArea) == 0:
+        pascalScore = 0
+    else:
+        pascalScore = intersectionArea/(computeArea(bb1)+computeArea(bb2)-intersectionArea)
 
     return pascalScore
