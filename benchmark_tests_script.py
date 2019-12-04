a6764d50bd5462a58af8913c29a9bb60327589c0
ddog <davidwscott9@gmail.com>
2019-12-04 13:58:56 -0500

    Updated comments

--- benchmark_tests_script.py
+++ benchmark_tests_script.py
@@ -1,7 +1,4 @@
 from benchmark_tests import benchmark_tests
 # This script runs the benchmark testing to generate success plots and obtain the FPS.
-# Available test_types are:
-# 'OTB50'
-# 'OTB100'
-# 'TC128'
-OP_vid, FPS_vid = benchmark_tests(test_type='OTB50')
+# Available test_types are: 'OTB50', 'OTB100', 'TC128'
+OP_vid, FPS_vid = benchmark_tests(test_type='OTB100')
