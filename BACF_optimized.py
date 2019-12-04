8e89d03eeb447b17c7155c31370b2eb49b79b120
ddog <davidwscott9@gmail.com>
2019-12-04 13:56:22 -0500

    some unnecessary lines and comments removed

--- BACF_optimized.py
+++ BACF_optimized.py
@@ -19,12 +19,13 @@ def BACF_optimized(params):
     from get_features import get_features
     from resp_newton import resp_newton
     from get_subwindow_no_window import get_subwindow_no_window
-    # Setting parameters for local use.
+
+    # Setting parameters
     search_area_scale = params['search_area_scale']  # size of training/detection area proportional to the target size
     output_sigma_factor = params['output_sigma_factor']
     learning_rate = params['learning_rate']
     filter_max_area = params['filter_max_area']
-    nScales = params['number_of_scales']
+    nScales = params['number_of_scales']  # number of scale resolutions to check
     scale_step = params['scale_step']
     interpolate_response = params['interpolate_response']
 
@@ -61,6 +62,7 @@ def BACF_optimized(params):
 
     # target size at the initial scale
     base_target_sz = target_sz / currentScaleFactor
+
     # window size, taking padding into account
     if params['search_area_shape'] == 'proportional':
         sz = np.floor(base_target_sz * search_area_scale)  # proportional area, same aspect ratio as the target
@@ -84,12 +86,12 @@ def BACF_optimized(params):
                  int(-1 * np.floor((use_sz[0] - 1) / 2)))
     cg = np.roll(np.arange(-1 * np.floor((use_sz[1] - 1) / 2), np.ceil((use_sz[1] - 1) / 2) + 1),
                  int(-1 * np.floor((use_sz[1] - 1) / 2)))
-    [rs, cs] = np.meshgrid(rg, cg)  # MAY BE ANOTHER CANDIDATE ERROR SOURCE
+    [rs, cs] = np.meshgrid(rg, cg)
     rs = rs.T
     cs = cs.T
     # y is the desired correlation response at each point within the size of the filter
     y = np.exp(-0.5 * ((np.power(rs, 2) + np.power(cs, 2)) / np.power(output_sigma, 2)))
-    yf = np.fft.fft2(y)  # fast fourier transform of y --> SAME AS MATLAB
+    yf = np.fft.fft2(y)  # fast fourier transform of y
 
     if interpolate_response == 1:
         interp_sz = use_sz * featureRatio
@@ -100,10 +102,9 @@ def BACF_optimized(params):
     term1 = np.array([np.hanning(use_sz[0])])
     term2 = np.array([np.hanning(use_sz[1])])
     cos_window = np.matmul(term1.T, term2)
-    cos_window = cos_window.astype('float32')  # --> SAME AS MATLAB
+    cos_window = cos_window.astype('float32')
 
     # Calculate feature dimension
-    # IF SOMETHING IS WRONG HERE, MIGHT BE ABLE TO CIRCUMVENT THE TRY/EXCEPT BY ENSURING IMAGE FILES ARE WELL FORMATTED
     try:
         im = cv2.imread(video_path + '/img/' + s_frames[0])
         im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -115,6 +116,7 @@ def BACF_optimized(params):
             im = cv2.imread(video_path + '/' + s_frames[0])
             im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
+    # Check to see if it is a color image or grayscale
     if im.shape[2] == 3:
         if np.all(np.equal(im[:,:,0], im[:,:,1])):
             colorImage = False
@@ -122,22 +124,19 @@ def BACF_optimized(params):
             colorImage = True
     else:
         colorImage = False
-
-    # Compute feature dimensionality
-    # MOST EVERYTHING IN THIS SECTION SEEMS TO HAVE NO VALUE
-    feature_dim = features['fparams']['nDim']
     
     if im.shape[2] > 1 and colorImage is False:
         im = im[:,:,0]
         im = im.reshape([im.shape[0], im.shape[1], 1])
-    
+
+    # create scale factors to check for object at various scale resolutions
     if nScales > 0:
         scale_exp = np.arange(-1 * np.floor((nScales - 1) / 2), np.ceil((nScales - 1) / 2) + 1)
         scaleFactors = scale_step ** scale_exp
         min_scale_factor = scale_step ** np.ceil(m.log(max(np.divide(5, sz))) / m.log(scale_step))
         max_scale_factor = scale_step ** np.floor(m.log(min(np.divide([im.shape[0],
                                                                        im.shape[1]],
-                                                                      base_target_sz))) / m.log(scale_step))  # --> SAME AS MATLAB
+                                                                      base_target_sz))) / m.log(scale_step))
 
     if interpolate_response >= 3:
         # pre-computes the grid that is used for score optimization
@@ -145,20 +144,19 @@ def BACF_optimized(params):
                      int(-1 * np.floor((use_sz[0] - 1) / 2)))
         kx = np.roll(np.arange(-1 * np.floor((use_sz[1] - 1) / 2), np.ceil((use_sz[1] - 1) / 2) + 1),
                      int(-1 * np.floor((use_sz[1] - 1) / 2)))  # --> SAME AS MATLAB
-        kx = kx.T  # MAKE SURE KX ACTUALLY AN NDARRAY AND NOT JUST 1D OTHERWISE TRANSPOSE WON"T WORK!
+        kx = kx.T
         newton_iterations = params['newton_iterations']
 
     # initialize the projection matrix (x,y,h,w)
     rect_position = np.zeros([num_frames, 4])
-    ## time = 0
 
     # allocate memory for multi-scale tracking
     multires_pixel_template = np.zeros([int(sz[0]), int(sz[1]), im.shape[2], nScales], dtype=np.uint8)
-    small_filter_sz = np.floor(base_target_sz / featureRatio)  # --> SAME AS MATLAB
+    small_filter_sz = np.floor(base_target_sz / featureRatio)
     start_time = time.time()
 
     loop_frame = 0
-    for frame in range(0, len(s_frames)):  # S_FRAMES NEEDS TO BE A NP ARRAY FOR .SIZE TO WORK
+    for frame in range(0, len(s_frames)):
         try:
             im = cv2.imread(video_path + '/img/' + s_frames[frame])
             im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -171,10 +169,10 @@ def BACF_optimized(params):
                 im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
         if im.shape[2] > 1 and colorImage is False:
-            im = im[:,:,0]
+            im = im[:, :, 0]
             im = im.reshape([im.shape[0], im.shape[1], 1])
 
-        # do not estimate translation and scaling on the first frame, since we just want to initialize the tracker there
+        # do not estimate translation and scaling on the first frame, since we are just initializing the tracker
         if frame > 0:
 
             # The filter is applied on multiple resolutions of the search area. This is used to determine any changes in
@@ -187,36 +185,32 @@ def BACF_optimized(params):
             xtf = np.zeros(feat_term2.shape, dtype=complex)
             for p in range(0, feat_term2.shape[2]):
                 for n in range(0, feat_term2.shape[3]):
-                    xtf[:,:,p,n] = np.fft.fft2(np.multiply(feat_term2[:,:,p,n], cos_window[:,:]))  # --> SAME AS MATLAB
-            responsef = np.sum(np.multiply(np.conj(g_f)[:,:,:,None], xtf), axis=2)  # --> SAME AS MATLAB
+                    xtf[:,:,p,n] = np.fft.fft2(np.multiply(feat_term2[:,:,p,n], cos_window[:, :]))
+            responsef = np.sum(np.multiply(np.conj(g_f)[:, :, :, None], xtf), axis=2)
 
             # if we undersampled features, we want to interpolate the response to have the same size as the image patch
             if interpolate_response == 2:
                 # use dynamic interp size
                 interp_sz = np.floor(y.shape * featureRatio * currentScaleFactor)
 
-            responsef_padded = resizeDFT2(responsef, interp_sz)  # --> SAME AS MATLAB
+            responsef_padded = resizeDFT2(responsef, interp_sz)
 
+            # Get the response in the spatial domain
             response = np.zeros(responsef_padded.shape)
-            # response in the spatial domain
             for n in range(0, responsef.shape[2]):
                 response[:,:,n] = np.real(np.fft.ifft2(responsef_padded[:,:,n]))  # MAY HAVE AN ISSUE HERE NOT BEING SYMMETRIC -- therefore added real --> SAME AS MATLAB :D :)
 
             # find maximum peak
             if interpolate_response == 3:
                 raise ValueError('Invalid parameter value for "interpolate_response"')
-            elif interpolate_response == 4:  # IF GETTING AN ERROR WITH SIND, MAY BE BECAUSE IT NEEDS TO INDEX AT SIND-1
+            elif interpolate_response == 4:
                 [disp_row, disp_col, sind] = resp_newton(response, responsef_padded, newton_iterations, ky, kx, use_sz)
 
             # Check if the target has completely gone off the frame
             if np.isnan(disp_row) or np.isnan(disp_col):
                 break
 
-            # MAY NOT NEED THIS  SECTION. TOO MANY CONVERSIONS. SKIPPING FOR NOW. SEEMS LIKE interpolate_response = 4 always
-            #else:
-               # [row, col, sind] = np.unravel_index(response.shape, find)
-
-            # calculate translation
+            # calculate translation vector
             if interpolate_response == 0 or 3 or 4:
                 translation_vec = np.round(np.array([disp_row, disp_col]) * featureRatio *
                                            currentScaleFactor * scaleFactors[sind])
@@ -239,15 +233,13 @@ def BACF_optimized(params):
             pos = pos + translation_vec
 
         # extract training sample image region
-        pixels = get_pixels(im, pos, np.round(sz*currentScaleFactor), sz)  # --> SAME AS MATLAB ON FRAME = 0
+        pixels = get_pixels(im, pos, np.round(sz*currentScaleFactor), sz)
 
-        # extract features and do windowing
-        # With numpy, passing a matrix with dimensions > 2 yields totally different results than in MATLAB. Need to
-        # loop it over 3 dimensional term so as to only pass through dims at a time.
-        feat_term, _ = get_features(pixels, features, global_feat_params, None)  # --> DISCREPANCY FROM MATLAB
+        # extract features and perform windowing
+        feat_term, _ = get_features(pixels, features, global_feat_params, None)
         xf = np.zeros([feat_term.shape[1], feat_term.shape[1], feat_term.shape[2]], dtype=complex)
         for n in range(0, feat_term.shape[2]):
-            xf[:,:,n] = np.fft.fft2(np.multiply(feat_term[:,:,n,0], cos_window[:,:]))
+            xf[:, :, n] = np.fft.fft2(np.multiply(feat_term[:, :, n, 0], cos_window[:, :]))
         if frame == 0:
             model_xf = xf
         else:
@@ -257,32 +249,34 @@ def BACF_optimized(params):
         g_f = g_f.astype('float32')
         h_f = g_f
         l_f = g_f
+
+        # parameters from the original paper
         mu = 1
         betha = 10
         mumax = 10000
         i = 1
 
         T = np.prod(use_sz)
-        S_xx = np.sum(np.multiply(model_xf.conj(), model_xf), axis=2)  # --> Same as Matlab
+        S_xx = np.sum(np.multiply(model_xf.conj(), model_xf), axis=2)
         params['admm_iterations'] = 2
 
         while i <= params['admm_iterations']:
             # Solve for G
-            B = S_xx + (T * mu)  # --> Same as Matlab
+            B = S_xx + (T * mu)
             S_lx = np.sum(np.multiply(model_xf.conj(), l_f), axis=2)
             S_hx = np.sum(np.multiply(model_xf.conj(), h_f), axis=2)
 
-            # equation (10)
-            g_f = (((1 / (T * mu)) * np.multiply(yf[:,:,None], model_xf)) - ((1 / mu) * l_f) + h_f) - \
-                  np.divide((((1 / (T * mu)) * np.multiply(model_xf, np.multiply(S_xx, yf)[:,:,None])) -
-                             ((1 / mu) * np.multiply(model_xf, S_lx[:,:,None])) +
-                             (np.multiply(model_xf, S_hx[:,:,None]))), B[:,:,None])
+            # equation (10) in original paper
+            g_f = (((1 / (T * mu)) * np.multiply(yf[:, :, None], model_xf)) - ((1 / mu) * l_f) + h_f) - \
+                  np.divide((((1 / (T * mu)) * np.multiply(model_xf, np.multiply(S_xx, yf)[:, :, None])) -
+                             ((1 / mu) * np.multiply(model_xf, S_lx[:, :, None])) +
+                             (np.multiply(model_xf, S_hx[:, :, None]))), B[:, :, None])
 
             # solve for H
-            # Equation (6)
+            # Equation (6) in original paper
             h = np.zeros([g_f.shape[0], g_f.shape[1], g_f.shape[2]], dtype=complex)
             for n in range(0, g_f.shape[2]):
-                h[:,:,n] = (T / ((mu * T) + params['admm_lambda'])) * np.fft.ifft2((mu * g_f[:,:,n]) + l_f[:,:,n])
+                h[:, :, n] = (T / ((mu * T) + params['admm_lambda'])) * np.fft.ifft2((mu * g_f[:, :, n]) + l_f[:, :, n])
 
             [sx, sy, h] = get_subwindow_no_window(h, np.floor(use_sz / 2), small_filter_sz)
             t = np.zeros([int(use_sz[0]), int(use_sz[1]), h.shape[2]], dtype=complex)
@@ -290,10 +284,10 @@ def BACF_optimized(params):
 
             h_f = np.zeros([t.shape[1], t.shape[1], t.shape[2]], dtype=complex)
             for n in range(0, t.shape[2]):
-                h_f[:,:,n] = np.fft.fft2(t[:,:,n])
+                h_f[:, :, n] = np.fft.fft2(t[:, :, n])
 
             # update L
-            l_f = l_f + (mu * (g_f - h_f))  # --> SAME AS MATLAB :)
+            l_f = l_f + (mu * (g_f - h_f))
 
             # update mu- betha = 10
             mu = min(betha * mu, mumax)
@@ -302,19 +296,19 @@ def BACF_optimized(params):
         target_sz = np.floor(base_target_sz * currentScaleFactor)
 
         # save position and calculate FPS
-        rect_position[loop_frame, :] = np.concatenate((pos[1::-1] - np.floor(target_sz[1::-1] / 2), target_sz[1::-1]))  # should be 1x4
+        rect_position[loop_frame, :] = np.concatenate((pos[1::-1] - np.floor(target_sz[1::-1] / 2), target_sz[1::-1]))
 
         elapsed = time.time() - start_time
 
         # visualization
-        if visualization == 1:  # SET ANOTHER INPUT IN RUN_BACF TO INCLUDE VISUALIZATION as 1 or 0
-            rect_position_vis = np.concatenate((pos[1::-1] - (target_sz[1::-1] / 2), target_sz[1::-1]))  # should be 1x4
+        if visualization == 1:
+            rect_position_vis = np.concatenate((pos[1::-1] - (target_sz[1::-1] / 2), target_sz[1::-1]))
             im_to_show = im / 255
             if im_to_show.shape[2] == 1:
                 im_to_show = np.tile(im_to_show, [1, 1, 3])  # if grayscale, ensure it plots image in grayscale
             if frame == 0:
                 fig, ax = plt.subplots(1, num='Tracking')
-                ax.imshow(im_to_show)  # MATLAB CODE HAS IMAGESC INSTEAD OF IMSHOW. HOPEFULLY NOT A BIG DIFFERENCE
+                ax.imshow(im_to_show)
                 rect = patches.Rectangle(rect_position_vis[0:2], rect_position_vis[2], rect_position_vis[3],
                                      linewidth=3, edgecolor='g', facecolor='none')
                 ax.add_patch(rect)
@@ -329,7 +323,7 @@ def BACF_optimized(params):
                 resp_sz = np.round(sz * currentScaleFactor * scaleFactors[scale_ind])
                 xs = np.floor(old_pos[1]) + (np.arange(1, resp_sz[1] + 1)) - np.floor(resp_sz[1] / 2)
                 ys = np.floor(old_pos[0]) + (np.arange(1, resp_sz[0] + 1)) - np.floor(resp_sz[0] / 2)
-                sc_ind = np.floor((nScales - 1) / 2)  # NOT INCLUDING PLUS 1 SINCE IT'S AN INDEX
+                sc_ind = np.floor((nScales - 1) / 2)
 
                 ax.imshow(im_to_show)
                 ax.axis('off')  # remove axis values
@@ -349,10 +343,7 @@ def BACF_optimized(params):
                 fig.show()
                 fig.canvas.draw()  # used to update the plot in real-time
 
-        # For debugging
         loop_frame += 1
-        # if frame == 20:  # For debugging. Stop after x iterations
-            # np.zeros(1,2)
 
     # Save results
     fps = loop_frame / elapsed
