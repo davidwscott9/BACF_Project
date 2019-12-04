0e3e6f0877d0a9deda9612d6e8b81d52323f8bbd
ddog <davidwscott9@gmail.com>
2019-12-04 13:05:08 -0500

    removed unnecessary code

--- get_features.py
+++ get_features.py
@@ -11,7 +11,6 @@ def get_features(image, features, gparams, fg_size):
     """
     import numpy as np
     from get_hog import get_hog
-    # IGNORING IF STATEMENT. ASSUMING IT JUST IS A PYTHON DICT
 
     if len(image.shape) == 2:
         image = image.reshape([image.shape[0], image.shape[1], 1])
@@ -21,12 +20,7 @@ def get_features(image, features, gparams, fg_size):
         num_images = 1
     else:
         [im_height, im_width, num_im_chan, num_images] = image.shape
-    colorImage = num_im_chan == 3
 
-    # compute the total dimension of all features
-    ## tot_feature_dim = 0
-
-    # ANOTHER USELESS LOOKING FOR LOOP
     tot_feature_dim = features['fparams']['nDim']
 
     if fg_size is None or (not fg_size is True):
@@ -36,7 +30,6 @@ def get_features(image, features, gparams, fg_size):
         else:
             fg_size = [np.floor(im_height / gparams['cell_size']), np.floor(im_width / gparams['cell_size'])]
 
-    # IGNORING CELL_SIZE < 0 CASE BECAUSE WHEN WOULD THAT HAPPEN???
     feature_image = get_hog(image, features['fparams'], gparams)
     if num_images == 1:
         feature_image = feature_image.reshape(feature_image.shape[0], feature_image.shape[1],
